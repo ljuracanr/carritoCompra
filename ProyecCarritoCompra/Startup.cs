81f78cfa5d3c7f61b9d6b390ecbdf55a790ac86d
ljuracanr <eduardo.juracan@gmail.com>
2018-04-14 07:57:40 -0600

    Agregar archivos de proyecto.

---
+++ ProyecCarritoCompra/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProyecCarritoCompra.Startup))]
+namespace ProyecCarritoCompra
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
